ed12696b534bb8fa8fca5b7408dd669d131150b3
Nethermaker <tsenne2@huskers.unl.edu>
2021-03-22 17:05:25 -0500

    Add code to step through a song

--- Assets/Scripts/SongController.cs
+++ Assets/Scripts/SongController.cs
@@ -8,19 +8,20 @@ using Melanchall.DryWetMidi.Interaction;
 public class SongController : MonoBehaviour
 {
     public string midiPath;
+    
+    [SerializeField()] private InstrumentController instrument;
+
+    public PlayMode playMode = PlayMode.Continuous;
+    public bool paused = false;
+    public float speed = 1;
+
+    private float startTime;
+    private float songTime = 0;
+    private int noteIndex = 0;
+
     private List<Note> notes;
     private TempoMap tempoMap;
 
-    [SerializeField()]
-    private InstrumentController instrument;
-
-    public bool paused = false;
-    public float speed = 1;
-    private float startTime;
-    private float songTime = 0;
-    private int currentNote = 0;
-
-    // Start is called before the first frame update
     void Start()
     {
         midiPath = "Assets\\Songs\\" + midiPath + ".mid";
@@ -30,23 +31,27 @@ public class SongController : MonoBehaviour
         notes = new List<Note>(midiFile.GetNotes());
 
         startTime = Time.time;
+
+        if (playMode == PlayMode.Stepped)
+            StartCoroutine(StepThroughSong());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!paused)
+        if (!paused && playMode == PlayMode.Continuous)
         {
             songTime += Time.deltaTime * speed;
+
+            // Find all notes that exist between the last frame and this frame and play them
             while (true)
             {
-                Note note = notes[currentNote];
-                float noteTime = note.TimeAs<MetricTimeSpan>(tempoMap).TotalMicroseconds / 1000000f;
+                Note note = notes[noteIndex];
+                float noteTime = GetNoteTime(note);
                 if (noteTime <= songTime)
                 {
                     instrument.PlayNote(note.NoteNumber);
-                    currentNote++;
-                    if (currentNote == notes.Count)
+                    noteIndex++;
+                    if (noteIndex == notes.Count)
                     {
                         paused = true;
                         break;
@@ -59,4 +64,48 @@ public class SongController : MonoBehaviour
             }
         }
     }
+
+    public void StepByAmount(int amount)
+    {
+        int direction = Math.Sign(amount);
+        Debug.Log(noteIndex);
+
+        for (int i = 0; i < amount; i++)
+        {
+            Note note;
+            float noteTime;
+
+            // All notes in a chord are considered to be part of the same "step"
+            do
+            {
+                noteIndex += direction;
+                note = notes[noteIndex];
+                noteTime = GetNoteTime(note);
+
+                instrument.PlayNote(note.NoteNumber);
+                songTime = noteTime;
+            } 
+            while (noteTime == GetNoteTime(notes[noteIndex + direction]));
+        }
+    }
+
+    private IEnumerator StepThroughSong()
+    {
+        while (true)
+        {
+            StepByAmount(1);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public float GetNoteTime(Note note)
+    {
+        return note.TimeAs<MetricTimeSpan>(tempoMap).TotalMicroseconds / 1000000f;
+    }
+}
+
+public enum PlayMode
+{
+    Continuous,
+    Stepped
 }
