e8249320ef570bd397ee5543ff25ee761b0db883
DocQuantum <robert.kirkp14@gmail.com>
2021-03-26 16:54:05 -0500

    Fixed mallet respond

--- Assets/Scripts/MalletController.cs
+++ Assets/Scripts/MalletController.cs
@@ -5,33 +5,48 @@ using Valve.VR.InteractionSystem;
 
 public class MalletController : MonoBehaviour
 {
-    private Transform transform;
+    private Vector3 position;
+    private Quaternion rotation;
+    private bool ShouldReturn = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform = GetComponent<Transform>();
+        Transform transform = GetComponent<Transform>();
+        position = transform.position;
+        rotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         Interactable interact = GetComponent<Interactable>();
         Hand attached = interact.attachedToHand;
-        if(attached == null)
+        if (attached != null)
         {
-            Debug.Log("HERE1");
+            Debug.Log("attached");
+            ShouldReturn = true;
             return;
         }
-        bool isAttached = attached.ObjectIsAttached(gameObject);
-        if (!isAttached)
+        else
         {
-
-            Debug.Log("HERE2");
-            GetComponent<Transform>().SetPositionAndRotation(transform.position, transform.rotation);
+            if (ShouldReturn)
+            {
+                Debug.Log("should return");
+                GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+                GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
+                GetComponent<Transform>().SetPositionAndRotation(position, rotation);
+                ShouldReturn = false;
+                return;
+            }
         }
+        //bool isAttached = attached.ObjectIsAttached(gameObject);
+        //if (!isAttached)
+        //{
+
+        //    Debug.Log("HERE2");
+        //    GetComponent<Transform>().SetPositionAndRotation(transform.position, transform.rotation);
+        //}
         //Transform current = GetComponent<Transform>();
         //if (current.position.y >=)
     }
