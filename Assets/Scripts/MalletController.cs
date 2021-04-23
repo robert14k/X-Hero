a1c531c16f4c1fda672c0bc8d2522d46d20cfb41
Nethermaker <tsenne2@huskers.unl.edu>
2021-04-23 15:59:40 -0500

    Clean up MalletController.cs

--- Assets/Scripts/MalletController.cs
+++ Assets/Scripts/MalletController.cs
@@ -7,64 +7,44 @@ public class MalletController : MonoBehaviour
 {
     private Vector3 startPosition;
     private Quaternion startRotation;
-    private bool ShouldReturn = false;
+    private Rigidbody rigidBody;
+
+    private Hand hand;
 
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
         startRotation = transform.rotation;
+
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Interactable interact = GetComponent<Interactable>();
-        Hand attached = interact.attachedToHand;
-        if (attached != null)
+        if (hand == null && transform.position.y < startPosition.y - 1)
         {
-            //if (!ShouldReturn)
-            //{
-                ShouldReturn = true;
-                //setHand(attached);
-            //}
-            return;
+            ResetPosition();
         }
-        else
-        {
-            if (ShouldReturn)
-            {
-                GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-                GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
-                transform.SetPositionAndRotation(startPosition, startRotation);
-                ShouldReturn = false;
-                return;
-            }
-            if(transform.position.y < startPosition.y - 1)
-            {
-                GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-                GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, 0f, 0f);
-                transform.SetPositionAndRotation(startPosition, startRotation);
-                return;
-            }
-        }
-        //bool isAttached = attached.ObjectIsAttached(gameObject);
-        //if (!isAttached)
-        //{
-
-        //    Debug.Log("HERE2");
-        //    GetComponent<Transform>().SetPositionAndRotation(transform.position, transform.rotation);
-        //}
-        //Transform current = GetComponent<Transform>();
-        //if (current.position.y >=)
     }
 
-    void setHand(Hand hand)
+    public void OnAttachedToHand(Hand hand)
     {
-        Transform handTransform = hand.transform;
-        Quaternion rot = handTransform.rotation * Quaternion.Euler(0, -75, 0);
-        transform.SetPositionAndRotation(handTransform.position + new Vector3(.2f, 0f, 0f), rot);
-        
+        this.hand = hand;
     }
 
+    public void OnDetachedFromHand(Hand hand)
+    {
+        this.hand = null;
+        ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        rigidBody.velocity = new Vector3(0f, 0f, 0f);
+        rigidBody.angularVelocity = new Vector3(0f, 0f, 0f);
+        transform.SetPositionAndRotation(startPosition, startRotation);
+        return;
+    }
 }
