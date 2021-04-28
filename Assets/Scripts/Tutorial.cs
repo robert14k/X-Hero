4be3849909002ba12a76c19b0d03f8d57649ff9e
Nethermaker <tsenne2@huskers.unl.edu>
2021-04-28 16:27:43 -0500

    Add slides to tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,37 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
     private SongController songController;
 
+    private Image image;
+
+    [SerializeField] private Sprite scaleImage;
+    [SerializeField] private Sprite lowCImage;
+    [SerializeField] private Sprite dImage;
+    [SerializeField] private Sprite eImage;
+    [SerializeField] private Sprite fImage;
+    [SerializeField] private Sprite gImage;
+    [SerializeField] private Sprite aImage;
+    [SerializeField] private Sprite bImage;
+    [SerializeField] private Sprite highCImage;
+
     void Start()
     {
         songController = SongController.Instance;
+
+        image = GetComponent<Image>();
     }
 
     void Update()
     {
+        image.enabled = false;
         if (!songController.midiPath.Contains("StreamingAssets/Songs/Tutorial.mid"))
         {
-            // Disable any tutorial elements before returning
             return;
         }
-
-        // We also need to ensure the tutorial can *only* be in the mode/speed that we want it to be in
+        image.enabled = true;
 
         // This might be better as an if/else-if chain
         switch (songController.noteIndex)
         {
+            case 0:
+                image.sprite = lowCImage;
+                break;
             case 1:
-                // Display slide 1 or something
+                image.sprite = dImage;
                 break;
             case 2:
-                // Display slide 2 or something
+                image.sprite = eImage;
                 break;
-            // et cetera
+            case 3:
+                image.sprite = fImage;
+                break;
+            case 4:
+                image.sprite = gImage;
+                break;
+            case 5:
+                image.sprite = aImage;
+                break;
+            case 6:
+                image.sprite = bImage;
+                break;
+            case 7:
+                image.sprite = highCImage;
+                break;
             default:
+                image.sprite = scaleImage;
                 break;
         }
     }
